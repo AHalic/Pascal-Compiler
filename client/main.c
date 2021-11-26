658f567033095be9ca2dfd8635cf6b88b69a83f7
AHalic <sophie.gama@aluno.ufes.br>
2021-11-26 14:51:19 -0300

    Delete C files

--- client/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "scanner.h"
-
-int main() {
-    printf("Example 1!\n");
-    scanner_load();
-
-    return 0;
-}
--- src/scanner.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "scanner.h"
-
-void scanner_load() {
-    printf("Loading scanner...\n");
-}
