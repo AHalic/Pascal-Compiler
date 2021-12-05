e48ffa550c5aca18d069b8f1124979821aaaf87b
AHalic <sophie.gama@aluno.ufes.br>
2021-12-05 18:49:48 -0300

    Adds syntatic error

---
+++ examples/synerr06.pas
@@ -0,0 +1,21 @@
+program posneg;
+uses crt;
+var 
+ no : integer;
+ do: string;
+begin
+ clrscr;
+  Write('Enger a number:');
+  readln(no);
+
+  if (no > 0) then
+   writeln('You enter Positive Number')
+  else
+    if (no < 0) then
+     writeln('You enter Negative number')
+    else
+      if (no = 0) then
+      writeln('You enter Zero');
+
+  readln;
+ end.
