84f14c71e60ced046956af82ff6cb4f5c9ff4b2f
AHalic <sophie.gama@aluno.ufes.br>
2022-02-12 19:24:57 -0300

    Changes repeat node to while node

--- src/ast/NodeKind.java
+++ src/ast/NodeKind.java
@@ -98,7 +98,7 @@ public enum NodeKind {
     },
     REPEAT_NODE {
         public String toString() {
-            return "repeat";
+            return "while";
         }
     },
     STR_VAL_NODE {
