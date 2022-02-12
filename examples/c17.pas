85850587c30acd9476795c081140ec47eb8b6f77
AHalic <sophie.gama@aluno.ufes.br>
2022-02-12 19:25:06 -0300

    Adds examples

---
+++ examples/c17.pas
@@ -0,0 +1,22 @@
+// exemplo de uso do if
+
+program ifelseChecking;
+var
+   { local variable definition }
+   a : integer;
+
+begin
+   a := 10;
+   (* check the boolean condition *)
+   if( a < 20 ) then
+   begin
+      (* if condition is true then print the following *)
+      a := 4;
+      a := a + 2;
+    end
+   
+   else
+      (* if condition is false then print the following *) 
+      a := 4;
+    // writeln('value of a is : ', a);
+end.
