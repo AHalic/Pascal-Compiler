b51c0a6c6332a81efd3fa69e80ba4de117b8c09f
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-02-13 00:41:05 -0300

    Limpeza de código e teste para else if

---
+++ examples/c20.pas
@@ -0,0 +1,30 @@
+// exemplo de uso do if
+
+program ifelseChecking;
+var
+   { local variable definition }
+   a : integer;
+
+begin
+   a := 10;
+   (* check the boolean condition *)
+   if( a < 20 ) then
+   begin
+      a := 4;
+      a := a + 2;
+    end
+   
+   else if (a < 10) then
+   begin
+      a := 4;
+   end
+   
+   else if (a < 10) then
+   begin
+      a := 4;
+   end
+
+   else
+      writeln('Nenhum')
+
+end.
--- src/checker/SemanticChecker.java
+++ src/checker/SemanticChecker.java
@@ -579,14 +579,13 @@ public class SemanticChecker extends PascalParserBaseVisitor<AST> {
     }
 
     @Override // IF expression THEN statement (: ELSE statement)?
-	public AST visitIfStatement(IfStatementContext ctx) {
-		// Analisa a expressão booleana.
+    public AST visitIfStatement(IfStatementContext ctx) {
+        // Analisa a expressão booleana.
         AST exprNode = visit(ctx.expression());
         AST thenNode = null;
         checkBoolExpr(ctx.IF().getSymbol().getLine(), "if", exprNode.type);
 
-        // System.out.println(ctx.statement());
-		if (ctx.ELSE() == null) {
+        if (ctx.ELSE() == null) {
             // Constrói o bloco de código do loop.
 
             if (ctx.statement(0).unlabelledStatement().structuredStatement() == null) {
@@ -597,18 +596,9 @@ public class SemanticChecker extends PascalParserBaseVisitor<AST> {
             }
 
             return AST.newSubtree(IF_NODE, NO_TYPE, thenNode, exprNode);
-		} 
+        } 
         else {
-			// Faz uma busca pelo token na lista de filhos.
-			TerminalNode elseToken = ctx.ELSE();
-			int elseIdx = -1;
-			// for (int i = 0; i < ctx.children.size(); i++) {
-			// 	if (ctx.children.get(i).equals(elseToken)) {
-			// 		elseIdx = i;
-			// 		break;
-			// 	}
-			// }
-            
+            // Faz uma busca pelo token na lista de filhos.
             if (ctx.statement(0).unlabelledStatement().structuredStatement() == null) {
                 thenNode = AST.newSubtree(BLOCK_NODE, NO_TYPE);
                 thenNode.addChild(visit(ctx.statement(0).unlabelledStatement()));
@@ -616,8 +606,8 @@ public class SemanticChecker extends PascalParserBaseVisitor<AST> {
                 thenNode = visit(ctx.statement(0).unlabelledStatement());
             }
 
-			// Cria o nó com o bloco de comandos do ELSE.
-			AST elseNode = null;
+            // Cria o nó com o bloco de comandos do ELSE.
+            AST elseNode = null;
             
             if (ctx.statement(1).unlabelledStatement().structuredStatement() == null) {
                 elseNode = AST.newSubtree(BLOCK_NODE, NO_TYPE);
@@ -626,9 +616,9 @@ public class SemanticChecker extends PascalParserBaseVisitor<AST> {
                 elseNode = visit(ctx.statement(1).unlabelledStatement());
             }
 
-			return AST.newSubtree(IF_NODE, NO_TYPE, exprNode, thenNode, elseNode);
-		}
-	}
+            return AST.newSubtree(IF_NODE, NO_TYPE, exprNode, thenNode, elseNode);
+        }
+    }
 
     //
     @Override
