25c9615eea10fe35518f1b2e99b1e14a96bfb698
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-02-15 20:23:56 -0300

    Adiciona não case sensitive e casos de erro semântico para arrays

---
+++ examples/semerr12.pas
@@ -0,0 +1,16 @@
+// Redefinição de identificador (Case sensitive)
+
+program Functions;
+
+function Acumulado(add1, add2: real) : real;
+begin
+    Acumulado := add1 + add2;
+end;
+
+var
+   a: real;
+   Acumulado: real;
+
+begin
+    a := Acumulado(1, 1);
+end.
