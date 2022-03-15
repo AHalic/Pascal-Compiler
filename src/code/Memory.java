6987f2c6156d983bd8599c6d51bc1e1d46a7c039
MarcoAOliari <marcoaoliari@gmail.com>
2022-03-15 20:35:54 -0300

    memory class

---
+++ src/code/Memory.java
@@ -0,0 +1,32 @@
+package code;
+
+import java.util.Vector;
+
+import tables.VarTable;
+
+@SuppressWarnings("serial")
+public final class Memory extends Vector<Word> {
+
+	public Memory(VarTable vt) {
+		for (int i = 0; i < vt.size(); i++) {
+			this.add(Word.fromInt(0));
+		}
+	}
+	
+	public void storei(int addr, int value) {
+		this.set(addr, Word.fromInt(value));
+	}
+	
+	public int loadi(int addr) {
+		return this.get(addr).toInt();
+	}
+	
+	public void storef(int addr, float value) {
+		this.set(addr, Word.fromFloat(value));
+	}
+	
+	public float loadf(int addr) {
+		return this.get(addr).toFloat();
+	}
+	
+}
