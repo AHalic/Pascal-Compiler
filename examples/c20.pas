4ca4ec1db79c2158aa37f20ed1af7e32af01df45
AHalic <sophie.gama@aluno.ufes.br>
2022-03-18 13:35:28 -0300

    adiciona caso de teste com if em função

---
+++ examples/c20.pas
@@ -0,0 +1,20 @@
+// Exemplo de programa usando função sem parâmetro
+
+program Functions;
+
+function Something: real;
+var
+    temp: integer;
+begin
+    Something := 1;
+    temp := 1;
+    if (temp >= 1) then
+        write('I did something');
+end;
+
+var
+    a: real;
+begin
+    // Something;
+    write('I will do something');
+end.
