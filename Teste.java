b0fd13fbd5c5a8dd3d3582c260533ce557a3f621
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2022-03-19 11:55:08 -0300

    Remove arquivo de teste

--- Teste.java
+++
@@ -1,9 +0,0 @@
-public class Teste {
-    public static void main(String args[]) {
-        double asd = 1.2;
-        if (2.0 > asd) {
-            System.out.println("asdasdas");
-        }
-        return;
-    }
-}
